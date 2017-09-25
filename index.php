744cbb1bd97de5bcd55c3b0fb5f43f8f68103875
blaauwkoen <blaauwkoen@gmail.com>
2017-09-25 16:13:02 +0200

    Changed extension

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <title>Koen Blaauw</title>
 <link href="css/style.css" type="text/css" rel="stylesheet">
 <link href="css/bootstrap-grid.css" type="text/css" rel="stylesheet">
-<script src="js/lastfm.js" type="text/javascript"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>
+<script src="js/lastfm.js" type="text/javascript"></script>
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:600,600i,700" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script>
